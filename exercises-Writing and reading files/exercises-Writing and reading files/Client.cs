414333e8cbc5a0cca5076c345bb308eff869cce1
FarrukhSadikhov <farrukh.sadikhov@outlook.com>
2022-02-08 17:53:15 +0400

    first-commit

---
+++ exercises-Writing and reading files/exercises-Writing and reading files/Client.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace exercises_Writing_and_reading_files
+{
+    class Client
+    {
+        public int Id { get; set; }
+        public int Passport { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Payment { get; set; }
+        public void Add_CSV()
+        {
+            string path = @"C:\Client";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            Console.WriteLine("Сколько клиентов хотите внести: ");
+            int n = Program.CheckNumber();
+            string[] arr = new string[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Введите Id: ");
+                Id = Program.CheckNumber();
+                Console.WriteLine("Введите номер паспорта  AZE/AA: ");
+                Passport = Program.CheckNumber();
+                Console.WriteLine("Введите имя:");
+                Name = Program.CheckString();
+                Console.WriteLine("Введите возраст: ");
+                Age = Program.CheckNumber();
+                Console.WriteLine("Введите платёж: ");
+                Payment = Program.CheckNumber();
+                arr[i] = $"Id:{Id}\nНомер паспорта:{Passport}\nИмя:{Name}\nВозраст:{Age}\nПлатёж:{Payment}\n";
+            }
+            using (FileStream fstream = new FileStream($"{path}\\note.csv", FileMode.OpenOrCreate)) { }
+            string writePath = @"C:\Client\note.csv";
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+
+                    using (StreamWriter sw = new StreamWriter(writePath, true, Encoding.UTF8))
+                    {
+                        sw.Write(arr[i]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Запись выполнена");
+        }
+        public void Read_CSV()
+        {
+            string path = @"C:\Client";
+            using (FileStream fstream = File.OpenRead($"{path}\\note.csv")) { }
+            string readPath = @"C:\Client\note.csv";
+            try
+            {
+                using (StreamReader sr = new StreamReader(readPath))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void Add_TXT()
+        {
+            string path = @"C:\Client";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            Console.WriteLine("Сколько клиентов хотите внести: ");
+            int n = Program.CheckNumber();
+            string[] arr = new string[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Введите Id: ");
+                Id = Program.CheckNumber();
+                Console.WriteLine("Введите номер паспорта  AZE/AA: ");
+                Passport = Program.CheckNumber();
+                Console.WriteLine("Введите имя:");
+                Name = Program.CheckString();
+                Console.WriteLine("Введите возраст: ");
+                Age = Program.CheckNumber();
+                Console.WriteLine("Введите платёж: ");
+                Payment = Program.CheckNumber();
+                arr[i] = $"Id:{Id}\nНомер паспорта:{Passport}\nИмя:{Name}\nВозраст:{Age}\nПлатёж:{Payment}\n";
+            }
+            using (FileStream fstream = new FileStream($"{path}\\note.txt", FileMode.OpenOrCreate)) { }
+            string writePath = @"C:\Client\note.txt";
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+
+                    using (StreamWriter sw = new StreamWriter(writePath, true, Encoding.UTF8))
+                    {
+                        sw.Write(arr[i]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Запись выполнена");
+        }
+        public void Read_TXT()
+        {
+            string path = @"C:\Client";
+            using (FileStream fstream = File.OpenRead($"{path}\\note.txt")) { }
+            string readPath = @"C:\Client\note.txt";
+            try
+            {
+                using (StreamReader sr = new StreamReader(readPath))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ exercises-Writing and reading files/exercises-Writing and reading files/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+
+namespace exercises_Writing_and_reading_files
+{
+    class Program
+    {
+        public static int CheckNumber()
+        {
+            int n;
+            do
+            {
+                if (Int32.TryParse(Console.ReadLine(), out n))
+                {
+                    return n;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка!!!Введите корректные данные");
+                }
+            }
+            while (true);
+        }
+        public static string CheckString()
+        {
+            string name;
+            string Error = "`1234567890-=[];' ,./~!@#$%^&*()_+{}:?";
+            bool Error2;
+            do
+            {
+                Error2 = true;
+                name = Console.ReadLine();
+                if (name == "")
+                {
+                    Error2 = false;
+                }
+                foreach (char item1 in name)
+                {
+                    foreach (char item2 in Error)
+                    {
+                        if (item1 == item2)
+                        {
+                            Error2 = false;
+                        }
+                    }
+                }
+                if (!Error2)
+                {
+                    Console.WriteLine("Ошибка! Повторите ввод:");
+                }
+            } while (!Error2);
+            name = name.ToLower();
+            return char.ToUpper(name[0]) + name.Substring(1);
+        }
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            bool finish = true;
+            do
+            {
+                Console.WriteLine("выберите действия");
+                Console.WriteLine("1 Добавить записи");
+                Console.WriteLine("2 Читать данные из файла");
+                Console.WriteLine("3 Выход из приложения");
+                Console.WriteLine("Выберите одно из выше указанных действий: ");
+                string number = Console.ReadLine();
+                try
+                {
+                    switch (number)
+                    {
+                        case "1":
+                            Console.WriteLine("1)Добавить кликента в текстовой файл нажмите\n2)Добавить кликента в Excel файл нажмите");
+                            number = Console.ReadLine();
+                            do
+                            {
+                                if (number == "1")
+                                {
+                                    client.Add_TXT();
+                                    break;
+                                }
+                                if (number == "2")
+                                {
+                                    client.Add_CSV();
+                                    break;
+                                }
+                                else { Console.WriteLine("Ошибка!!!Введите 1 или 2"); }
+                                break;
+                            } while (true);
+                            break;
+                        case "2":
+                            Console.WriteLine("1)Читать данные из текстового файла нажмите\n2)Читать данные из Excel файла нажмите");
+                            number = Console.ReadLine();
+                            do
+                            {
+                                if (number == "1")
+                                {
+                                    client.Read_TXT();
+                                    break;
+                                }
+                                if (number == "2")
+                                {
+                                    client.Read_CSV();
+                                    break;
+                                }
+                                else { Console.WriteLine("Ошибка!!!Введите 1 или 2"); }
+                                break;
+                            } while (true);
+                            break;
+                        case "3":
+                            Console.WriteLine("Выход из приложения");
+                            finish = false;
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Ошибка!!!Введите одно из выше указанных действий");
+                }
+            }
+            while (finish == true);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
